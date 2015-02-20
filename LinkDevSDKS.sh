f48b20e0579117e3de2bae17914beab0f06af3c0
Mitch Amiano <mcamiano@ncsu.edu>
2015-02-19 19:59:25 -0500

    Apply python patch per wiki

---
+++ LinkDevSDKS.sh
@@ -0,0 +1,4 @@
+mkdir /Developer
+cd /Developer
+ln -s /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/Library
+ln -s /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs
